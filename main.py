ed5d7df4ce5dc0e7a2e5bf50bb8fb5fafb569348
Eqrazer <89294030+Eqrazer@users.noreply.github.com>
2022-03-01 00:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ async def start_bot():
     await bot.stop()
 
     
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(start_bot())
-asyncio.run(start_bot())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
